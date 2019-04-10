e8b43e4eee57ea942b27bec1d17e329c14c23bdd
npv34 <phanvanluan611996@gmail.com>
2019-04-10 17:33:03 +0700

    create migration user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('image')->nullable();
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
